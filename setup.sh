58ad23726382f338e8762e5c43c92f03e7136a3b
adadlani <anel.dadlani@gmail.com>
2017-06-05 18:32:49 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Sample script to build a Django development box
+
+# Install Anaconda Anaconda3-4.4.0-Linux-x86_64.sh
+
+# Setup environment to pick up latest Python/PIP
+
+# PIP install django, mod_wsgi-httpd
+
+# Create sample Django project and perform initial setup
+
+# Run Django project
