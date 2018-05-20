98e899d5e76f8bee5c288bffe78ccf943665693c
Anel Dadlani <anel.dadlani@gmail.com>
2018-05-20 18:52:56 -0400

    Fix white space to be 4 spaces

--- get_cve_data.py
+++ get_cve_data.py
@@ -10,14 +10,14 @@ NVD_CVE_ENDPOINT = 'https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-mo
 DATA_FILE = './data.json.gz'
 
 with open(DATA_FILE, 'wb') as fd:
-	c = pycurl.Curl()
-	c.setopt(c.URL, NVD_CVE_ENDPOINT)
-	c.setopt(c.WRITEDATA, fd)
-	c.perform()
+    c = pycurl.Curl()
+    c.setopt(c.URL, NVD_CVE_ENDPOINT)
+    c.setopt(c.WRITEDATA, fd)
+    c.perform()
 
 if c.getinfo(c.RESPONSE_CODE) != 200:
-	print('data request failed')
-	exit(1)
+    print('data request failed')
+    exit(1)
 
 # Close Curl
 c.close()
@@ -29,12 +29,12 @@ with gzip.open(DATA_FILE, 'rt') as fd:
 	data = json.load(fd)
 
 # Read in-memory JSON structure keys
-#	CVE_data_version
-#	CVE_data_format
-#	CVE_Items
-#	CVE_data_timestamp
-#	CVE_data_numberOfCVEs
-#	CVE_data_type
+#   CVE_data_version
+#   CVE_data_format
+#   CVE_Items
+#   CVE_data_timestamp
+#   CVE_data_numberOfCVEs
+#   CVE_data_type
 print('CVE_data_version:', data['CVE_data_version'])
 print('CVE_data_format:', data['CVE_data_format'])
 print('CVE_data_timestamp:', data['CVE_data_timestamp'])
@@ -43,8 +43,9 @@ print('CVE_data_type:', data['CVE_data_type'])
 
 count = 0
 for item in data['CVE_Items']:
-	if 'cve' in item:
-		if 'CVE_data_meta' in item['cve']:
-			if 'ID' in item['cve']['CVE_data_meta']:
-				#print(item['cve']['CVE_data_meta']['ID'])
-				count = count + 1
+    if 'cve' in item:
+        if 'CVE_data_meta' in item['cve']:
+            if 'ID' in item['cve']['CVE_data_meta']:
+                #print(item['cve']['CVE_data_meta']['ID'])
+                count = count + 1
+
